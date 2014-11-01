9f5163ea17775909799df8ada90fee123a0c0e33
Andy Walz <andywalz@gmail.com>
2014-11-01 16:34:49 -0500

    Initial Commit

---
+++ MN Solar App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MN Solar App
+//
+//  Created by Andy Walz on 11/1/14.
+//  Copyright (c) 2014 MN Solar Suitability Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MN Solar AppTests/MN_Solar_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  MN_Solar_AppTests.m
+//  MN Solar AppTests
+//
+//  Created by Andy Walz on 11/1/14.
+//  Copyright (c) 2014 MN Solar Suitability Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MN_Solar_AppTests : XCTestCase
+
+@end
+
+@implementation MN_Solar_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
