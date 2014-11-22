13f483ac088394ec5388a5de77786632cbcb5396
Chris Martin <cmartin616@gmail.com>
2014-11-21 21:43:37 -0600

    Built test database.  Branch is temporary other than DBmanager.h/.m

---
+++ MN Solar App/EditInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditInfoViewController.h
+//  MN Solar App
+//
+//  Created by Chris Martin on 11/21/14.
+//  Copyright (c) 2014 MN Solar Suitability Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditInfoViewController : UIViewController
+
+@end
