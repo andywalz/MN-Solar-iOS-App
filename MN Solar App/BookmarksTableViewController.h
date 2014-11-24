fa8d6ba8e876c5b3eea0d019f926a6bcf2833555
Chris Martin <cmartin616@gmail.com>
2014-11-23 22:00:43 -0600

    Working on bookmarks

--- MN Solar App/BookmarksTableViewController.h
+++ MN Solar App/BookmarksTableViewController.h
@@ -1,13 +1,30 @@
 //
+
 //  BookmarksTableViewController.h
+
 //  MN Solar App
+
 //
+
 //  Created by Andy Walz on 11/2/14.
+
 //  Copyright (c) 2014 MN Solar Suitability Team. All rights reserved.
+
 //
+
+
 
 #import <UIKit/UIKit.h>
 
-@interface BookmarksTableViewController : UITableViewController
+
+
+@interface BookmarksTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+
+@property (strong, nonatomic) UITableViewCell *cell;
+
+
 
 @end
+
--- MN Solar App/BookmarksTableViewController.m
+++ MN Solar App/BookmarksTableViewController.m
@@ -1,10 +1,18 @@
 //
+
 //  BookmarksTableViewController.m
+
 //  MN Solar App
+
 //
+
 //  Created by Andy Walz on 11/2/14.
+
 //  Copyright (c) 2014 MN Solar Suitability Team. All rights reserved.
+
 //
+
+
 
 #import "BookmarksTableViewController.h"
 
@@ -14,87 +22,178 @@
 
 @implementation BookmarksTableViewController
 
+{
+    
+    NSArray *tableData;
+    
+}
+
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     
+    tableData = [NSArray arrayWithObjects:@"Andy Walz", @"Chris Martin", @"Katie Menk", @"Yuanyuan Luo", nil];
+    
     // Uncomment the following line to preserve selection between presentations.
+    
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
 }
 
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Potentially incomplete method implementation.
+    
+    //#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 1;
+    
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//#warning Incomplete method implementation.
+    
+    //#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 3;
+    return [tableData count];
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
     
-    return cell;
+    //UITableView *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    self.cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:simpleTableIdentifier];
+    
+    /*if (self.cell == nil){
+     
+     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+     
+     }*/
+    
+    self.cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    self.cell.detailTextLabel.text = @"Test description";
+    
+    //cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return self.cell;
+    
 }
-*/
+
+-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    //UIAlertView *messageAlert = [[UIAlertView alloc]initWithTitle:@"Row Selected" message:@"You've selected a row" delegate:nil cancelButtonTitle:@"OK"otherButtonTitles:@"Cancel", nil];
+    
+    // Display alert
+    //[messageAlert show];
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType =UITableViewCellAccessoryDetailButton;
+    
+}
+
+-(void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"You pressed the arrow!");
+}
+
+
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+ 
+ // Override to support conditional editing of the table view.
+ 
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ 
+ // Return NO if you do not want the specified item to be editable.
+ 
+ return YES;
+ 
+ }
+ 
+ */
+
+
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ 
+ // Override to support editing the table view.
+ 
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ 
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ 
+ // Delete the row from the data source
+ 
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ 
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ 
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ 
+ }
+ 
+ }
+ 
+ */
+
+
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+ 
+ // Override to support rearranging the table view.
+ 
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ 
+ }
+ 
+ */
+
+
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ 
+ // Override to support conditional rearranging of the table view.
+ 
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ 
+ // Return NO if you do not want the item to be re-orderable.
+ 
+ return YES;
+ 
+ }
+ 
+ */
+
+
 
 /*
-#pragma mark - Navigation
+ 
+ #pragma mark - Navigation
+ 
+ 
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ 
+ // Get the new view controller using [segue destinationViewController].
+ 
+ // Pass the selected object to the new view controller.
+ 
+ }
+ 
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
